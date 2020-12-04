95fbeee583c62de4c6f3884fef634fbdd6ee9e7a
wynn-w <wwwynn22@gmail.com>
2020-12-04 17:46:13 +0800

    设置了行内元素垂直对齐方式为middle

---
+++ src/components/buttonGroup.vue
@@ -0,0 +1,43 @@
+<!--
+ * @Description: 
+ * @Author: wynn-w
+ * @Date: 2020-12-03 20:41:33
+ * @Do not edit: 2020-12-03 21:10:00
+ * @LastEditors: wynn-w
+-->
+<template>
+  <div class="j-button-group">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "g-button-group",
+    props:[]
+}
+</script>
+
+<style lang="scss">
+.j-button-group{
+  display: inline-flex;
+  vertical-align: middle;
+  > .j-button {
+    border-radius: 0;
+    &:not(:last-child){
+      border-top: 0;
+      border-bottom: 0;
+      border-right: .1em solid rgba(255,255,255,.9);
+    }
+    &:first-child{
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+    &:last-child{
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+
+  }
+}
+</style>
