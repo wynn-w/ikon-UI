397fc9225cdfe67b35c6c4dab49460c4fffe031c
wynn-w <wwwynn22@gmail.com>
2020-12-08 23:20:18 +0800

    修改了前面的测试用例，新增点击事件的测试用例

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Author: wynn-w
  * @Description: 
  * @Date: 2020-12-03 10:51:31
- * @LastEditTime: 2020-12-07 23:35:07
+ * @LastEditTime: 2020-12-08 23:17:26
  * @LastEditors: wynn-w
  */
 import Vue from 'vue';
@@ -17,42 +17,82 @@ new Vue({
 }).$mount('#app')
 // 单元测试
 import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies)
 let expect = chai.expect
 {
+    const div = document.createElement("div")
+    const test = document.getElementById("test")
+    document.body.appendChild(div)
     const Constructor = Vue.extend(Button)
     const button = new Constructor({
         propsData: {
-            icon: "download",
+            icon: "download"
         }
     })
-    button.$mount()
+    button.$mount(div)
     const href = button.$el.querySelector("use").getAttribute('xlink:href')
     expect(href).to.eq("#i-download");
+    const ariaHidden = button.$el.querySelector("svg").getAttribute('aria-hidden');
+    expect(ariaHidden).to.eq("true");
+    button.$el.remove()
+    button.$destroy()
+
 }
 {
+    const div = document.createElement("div")
+    const test = document.getElementById("test")
+    document.body.appendChild(div)
     const Constructor = Vue.extend(Button)
     const button = new Constructor({
         propsData: {
-            icon: "download",
+            icon: "delete",
             circle: true
         }
     })
-    button.$mount("#test")
-    const href = button.$el.querySelector("use").getAttribute('xlink:href')
-    expect(href).to.eq("#i-download");
-    const ariaHidden = button.$el.querySelector("svg").getAttribute('aria-hidden'); 
-    expect(ariaHidden).to.eq("true");
+    button.$mount(div)
+    const [ClassName] = button.$el.querySelector("svg").parentNode.className.match(/circle/g)
+    expect(ClassName).to.equal("circle");
+    button.$el.remove()
+    button.$destroy()
+
 }
+
 {
+    const div = document.createElement("div")
+    const test = document.getElementById("test")
+    document.body.appendChild(div)
     const Constructor = Vue.extend(Button)
     const button = new Constructor({
         propsData: {
-            circle: true,
-            loading: true
+            loading: true,
+            circle: true
         }
     })
-    button.$mount("#test")
-    const alignItems = button.$el.querySelector("svg").getAttribute('align-items'); 
-    expect(alignItems).to.eq(null);
+    button.$mount(div)
+    const [ClassName] = button.$el.querySelector("svg").className.baseVal.match(/loading/g)
+    // console.log(ClassName);
+    expect(ClassName).to.equal("loading");
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const div = document.createElement("div")
+    const test = document.getElementById("test")
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            loading: true,
+        }
+    })
+    button.$mount(div)
+    // 引入spies做间谍函数并监听它，如果被调用则说明点击测试通过
+    const spy = chai.spy(function () { })
+    button.$on('click', spy())
+    button.$el.click()
+    expect(spy).to.have.been.called();
+    button.$el.remove()
+    button.$destroy()
 }
 
