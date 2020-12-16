d8ab3ad0106dc170e8bfbcd6c375db7552ddc3f2
wynn-w <wwwynn22@gmail.com>
2020-12-16 23:34:20 +0800

    刷新一下

--- karma.conf.js
+++ karma.conf.js
@@ -1,52 +1,84 @@
-"use strict";
-
 /*
  * @Author: wynn-w
  * @Description: 
  * @Date: 2020-12-09 14:47:17
- * @LastEditTime: 2020-12-09 18:31:02
+ * @LastEditTime: 2020-12-16 22:50:03
  * @LastEditors: wynn-w
  */
 module.exports = function (config) {
   config.set({
+
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha', 'sinon-chai'],
     client: {
+      captureConsole: true,
       chai: {
         includeStack: true
       }
     },
+    // client: {
+    //   captureConsole: true,
+    //   mocha: {
+    //     bail: true
+    //   }
+    // }
+
     // list of files / patterns to load in the browser
-    files: ['dist/**/*.test.js', 'dist/**/*.test.css'],
+    files: [
+      'dist/*.test.css',
+      'dist/*.test.js',
+    ],
+
+
     // list of files / patterns to exclude
     exclude: [],
+
+
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {},
+
+
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
+
+
     // web server port
     port: 9876,
+
+
     // enable / disable colors in the output (reporters and logs)
     colors: true,
+
+
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
+
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
+
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'Chrome_without_security'],
+
+    customLaunchers: {
+      Chrome_without_security: {
+        base: 'Chrome',
+        flags: ['--disable-web-security', '--disable-site-isolation-trials']
+      }
+    },
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
+    detached: false,
     // Concurrency level
     // how many browser should be started simultaneous
     concurrency: Infinity
-  });
-};
+  })
+}
