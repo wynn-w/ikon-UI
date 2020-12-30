3930d4bc3597b6d4573e2dd518c8e1949d3c1aac
wynn-w <wwwynn22@gmail.com>
2020-12-30 23:55:39 +0800

    新增 col 组件

---
+++ src/components/col.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="col" :class="[`col-${span}`, offset && `offset-${offset}`]">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IkCol",
+  props: {
+    span: {
+      type: [String, Number],
+    },
+    offset: {
+      type: [String, Number],
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.col {
+  min-height: 36px;
+  width: 50%;
+  border-radius: 0.2em;
+  $class-prefix: col-;
+  @for $num from 1 through 24 {
+    &.#{$class-prefix}#{$num} {
+      width: $num / 24 * 100%;
+    }
+  }
+  $class-prefix: offset-;
+  @for $num from 1 through 24 {
+    &.#{$class-prefix}#{$num} {
+      margin-left: $num / 24 * 100%;
+    }
+  }
+}
+</style>
