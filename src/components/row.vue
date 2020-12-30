e1fc11189143dc0fd55bdbe1184217c2aea4808c
wynn-w <wwwynn22@gmail.com>
2020-12-30 23:53:55 +0800

    新增 row 组件

---
+++ src/components/row.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row" >
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "IkRow"
+}
+</script>
+
+<style lang="scss">
+.row{
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  width: 100%;
+}
+
+</style>
