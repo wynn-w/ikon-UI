84b6803cb6bd8f84c13c201893ce2f5b85c62027
wynn-w <wwwynn22@gmail.com>
2021-01-07 16:57:47 +0800

    修改：添加了基础样式

--- src/components/toast.vue
+++ src/components/toast.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="toast"></div>
+  <div class="toast">
+      <slot></slot>
+  </div>
 </template>
 
 <script>
@@ -9,5 +11,22 @@ export default {
 </script>
 
 <style lang="scss">
-    .toast{}
+    $font-size: 14px;
+    $toast-height: 1.8;
+    $toast-bg-color: rgba(0,0,0,.75);
+    $toast-color:#ffffff;
+    .toast{
+        font-size: $font-size;
+        line-height: $toast-height;
+        background-color: $toast-bg-color;
+        color: $toast-color;
+        box-shadow: 0 0 4px 0 rgba(0, 0, 0, .5);
+        padding: 0 16px;
+        border-radius: 4px;
+        align-items: center;
+        position: fixed;
+        top:0;
+        left: 50%;
+        transform: translateX(-50%);
+    }
 </style>
