8a4a1d1e0c89f85f6e6e91c251a72203876d7960
wynn-w <wwwynn22@gmail.com>
2021-01-09 00:32:11 +0800

    重构了代码，并实现页面同时有且仅有一个 toast 组件

--- src/plug/toast.plug.js
+++ src/plug/toast.plug.js
@@ -1,14 +1,22 @@
 import Toast from '../components/toast'
+function creatToast(Vue,message,propsData) {
+    const Constructor = Vue.extend(Toast)
+    const toast = new Constructor({ propsData })
+    toast.$slots.default = [message]
+    toast.$mount()
+    document.body.appendChild(toast.$el)
+    return toast
+}
+let currentToast
 export default {
     install(Vue) {
         Vue.prototype.$toast = (message = 'this is toast', toastProps) => {
-            const Constructor = Vue.extend(Toast)
-            const toast = new Constructor({
-                propsData: toastProps
-            })
-            toast.$slots.default = [message]
-            toast.$mount()
-            document.body.appendChild(toast.$el)
+            console.log(currentToast);
+            if(currentToast){
+            currentToast.close()
+            currentToast.$destroy()
+            }    
+            currentToast =  creatToast(Vue,message , toastProps)
         }
     }
 }
