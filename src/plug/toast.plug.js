22e60864e0ff4a10a634c102aae9c4ce57cbb516
wynn-w <wwwynn22@gmail.com>
2021-01-08 23:32:13 +0800

    修改：设值了用户未传文本时的默认值值

--- src/plug/toast.plug.js
+++ src/plug/toast.plug.js
@@ -1,9 +1,11 @@
 import Toast from '../components/toast'
 export default {
-    install (Vue) {
-        Vue.prototype.$toast = (message) => {
+    install(Vue) {
+        Vue.prototype.$toast = (message = 'this is toast', toastProps) => {
             const Constructor = Vue.extend(Toast)
-            const toast = new Constructor()
+            const toast = new Constructor({
+                propsData: toastProps
+            })
             toast.$slots.default = [message]
             toast.$mount()
             document.body.appendChild(toast.$el)
