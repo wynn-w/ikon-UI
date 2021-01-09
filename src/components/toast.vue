b9f080291691435f4c6ecca929c2c6514cb163fb
wynn-w <wwwynn22@gmail.com>
2021-01-09 23:56:16 +0800

    新增：添加了弹入效果，修改了 toast 组件在顶部、中间、底部出现时的样式

--- src/components/toast.vue
+++ src/components/toast.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="toast" :class="toastClasses">
-    <div class="toast__message">
-      <slot v-if="!enableHTML"></slot>
-      <div v-else v-html="$slots.default[0]" class="innerHTML"></div>
+  <div class="toast-wrapper" :class="toastClasses">
+    <div class="toast">
+      <div class="toast__message">
+        <slot v-if="!enableHTML"></slot>
+        <div v-else v-html="$slots.default[0]" class="innerHTML"></div>
+      </div>
+      <div class="division-line" ref="line"></div>
+      <span v-if="closeButton" class="close" @click="onClickBtn">
+        {{ closeButton.text }}
+      </span>
     </div>
-    <div class="division-line" ref="line"></div>
-    <span v-if="closeButton" class="close" @click="onClickBtn">
-      {{ closeButton.text }}
-    </span>
   </div>
 </template>
 
@@ -75,7 +77,7 @@ export default {
         this.timer = setTimeout(() => {
           this.close();
           this.autoClose = false;
-        }, this.autoCloseDelay * 1000);
+        }, this.autoCloseDelay);
       }
     },
   },
@@ -97,44 +99,94 @@ export default {
 <style lang="scss">
 $font-size: 14px;
 $toast-min-height: 2em;
-$toast-bg-color: rgba(0, 0, 0, 0.75);
+$toast-bg-color: rgba(0, 0, 0, .75);
 $toast-color: #ffffff;
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+    filter:alpha(opacity=0);
+  }
+  100% {
+    opacity: 1;
+    filter:alpha(opacity=100);
+  }
+}
+@keyframes fade-up {
+  0% {
+    opacity: 0;
+    filter:alpha(opacity=0);
+    transform: translateY(100%);
+  }
+  100% {
+    opacity: 1;
+    filter:alpha(opacity=100);
+    transform: translateY(0%);
+  }
+}
+@keyframes fade-down {
+  0% {
+    opacity: 0;
+    filter:alpha(opacity=0);
+    transform: translateY(-100%);
+  }
+  100% {
+    opacity: 1;
+    filter:alpha(opacity=100);
+    transform: translateY(0%);
+  }
+}
+.toast-wrapper {
+  position: fixed;
+  left: 50%;
+  &.position-top {
+    top: 0;
+    transform: translateX(-50%);
+    .toast {
+      border-top-left-radius: 0;
+      border-top-right-radius: 0;
+      animation: fade-down 300ms;
+    }
+  }
+  &.position-middle {
+    top: 50%;
+    transform: translate(-50%);
+    .toast {
+      animation: fade-in 300ms;
+    }
+  }
+  &.position-bottom {
+    bottom: 0;
+    transform: translate(-50%);
+    .toast {
+      border-bottom-left-radius: 0;
+      border-bottom-right-radius: 0;
+      animation: fade-up 300ms;
+    }
+  }
+}
 .toast {
   font-size: $font-size;
   min-height: $toast-min-height;
   line-height: 1.8em;
   background-color: $toast-bg-color;
   color: $toast-color;
-  box-shadow: 0 0 4px 0 rgba(0, 0, 0, 0.5);
-  padding: 0 16px;
-  border-radius: 4px;
-  align-items: center;
+  box-shadow: 0 0 .3em 0 rgba(0, 0, 0, .5);
+  padding: 0 1em;
+  border-radius: .3em;
   display: flex;
-  position: fixed;
-  left: 50%;
-  transform: translateX(-50%);
+  align-items: center;
   .toast__message {
-    padding: 0.8em 0;
+    padding: .8em 0;
   }
   .division-line {
     height: 100%;
-    border-left: 0.1em solid #666;
+    border-left: .1em solid #666;
     margin-left: 1em;
   }
   .close {
     padding-left: 1em;
     flex-shrink: 0;
     cursor: default;
-  }
-  &.position-top {
-    top: 0;
-  }
-  &.position-middle {
-    top: 50%;
-    transform: translate(-50%, -50%);
-  }
-  &.position-bottom {
-    bottom: 0;
   }
 }
 </style>
