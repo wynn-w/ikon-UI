bf896c4e809b3f5176be5466468c868a4a13b448
wynn-w <wwwynn22@gmail.com>
2021-01-15 23:55:03 +0800

    新增：初始化了 tabs 组件及其子组件

---
+++ src/components/tables/tabs.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="tabs">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'IkTabs'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/tables/tabsHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="tabs-header">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'IkTabsHeader'
+}
+</script>
+
+<style>
+
+</style>
