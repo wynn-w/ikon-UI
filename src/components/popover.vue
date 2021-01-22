6e11abfb774377706cc9613f164e23f43f2a7a82
wynn-w <wwwynn22@gmail.com>
2021-01-22 22:04:42 +0800

    新增：初始化 popover 组件

---
+++ src/components/popover.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="ik-popover">
+    <div class="ik-popover__content-wrapper" v-if="visiable">
+      <slot name="content"></slot>
+    </div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IkPopover",
+  data() {
+    return {
+      visiable: true,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.ik-popover {
+  display: inline-block;
+  vertical-align: top;
+  position: relative;
+  > .ik-popover__content-wrapper {
+    position: absolute;
+    bottom: 100%;
+    left: 0;
+    border: 1px solid #dcdfe6;
+    border-radius: 0.3571rem;
+    box-shadow: 0 0.1429rem 0.8571rem 0 #dcdfe6
+  }
+}
+</style>
