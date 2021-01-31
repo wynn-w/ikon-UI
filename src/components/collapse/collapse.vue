698e1b9a153d915a70043201dcda0ffb359cd235
wynn-w <wwwynn22@gmail.com>
2021-01-31 23:59:50 +0800

    新增： 初始化了组件

---
+++ src/components/collapse/collapse.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="ik-collapse">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ikCollapse",
+};
+</script>
+
+<style lang="scss">
+    .ik-collapse{
+        border: 0.0714rem solid #ddd;
+        border-radius: 0.2857rem;
+    }
+</style>
---
+++ src/components/collapse/collapseItem.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="ik-collapse-item">
+    <div class="ik-collapse-item__title">
+        {{title}}
+    </div>
+    <div class="ik-collapse-item__content"></div>
+    <slot></slot>
+</div>
+  
+</template>
+
+<script>
+export default {
+    name:"ikCollapsItem",
+    props:{
+        title:{
+            type: String,
+            required:true
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+$border-color: #ddd;
+$border-radius: 4px;
+.ik-collapse-item{
+    > .title{
+        display: flex;
+        min-height: 32px;
+        margin:  -1px;
+        padding:  0 8px;
+        border: 1px solid $border-color;
+        align-items: center;
+    }
+    &:first-child{
+        >.title{
+            border-top-left-radius: $border-radius;
+            border-top-right-radius: $border-radius;
+        }
+    }
+    &:last-child{
+        >.title:last-child{
+            border-bottom-left-radius: $border-radius;
+            border-bottom-right-radius: $border-radius;
+        }
+    }
+}
+</style>
