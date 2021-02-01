993c564f2f47b0dc7c2f9efc5ae1fa70787d8852
wynn-w <wwwynn22@gmail.com>
2021-02-01 17:59:52 +0800

    新增：完成 single 部份功能实现（只展示唯一一个 item 的内容）；优化了显示效果

--- src/components/collapse/collapse.vue
+++ src/components/collapse/collapse.vue
@@ -5,14 +5,35 @@
 </template>
 
 <script>
+import Vue from "vue";
 export default {
   name: "ikCollapse",
+  props: {
+    single: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  provide() {
+    if (this.single) {
+      return { eventBus: this.eventBus };
+    }
+  },
+  data() {
+    return {
+      eventBus: new Vue(),
+    };
+  },
+  methods: {},
 };
 </script>
 
 <style lang="scss">
-    .ik-collapse{
-        border: 0.0714rem solid #ddd;
-        border-radius: 0.2857rem;
-    }
+.ik-collapse {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-top: 0.0714rem solid #ddd;
+  border-bottom: 0.0714rem solid #ddd;
+}
 </style>
--- src/components/collapse/collapseItem.vue
+++ src/components/collapse/collapseItem.vue
@@ -1,49 +1,100 @@
 <template>
-<div class="ik-collapse-item">
-    <div class="ik-collapse-item__title">
-        {{title}}
+  <div class="ik-collapse-item" :data-itemName="name">
+    <div class="ik-collapse-item__title" @click="onClick">
+        <div class="title-content">
+            <slot name="title">{{ title }}</slot>
+        </div>
+      
+      <div class="image-wrapper" ref="icon">
+          <ik-icon name="right"  class="image-content" color="#373133"><ik-icon>
+      </div>
     </div>
-    <div class="ik-collapse-item__content"></div>
-    <slot></slot>
-</div>
-  
+    <div class="ik-collapse-item__content" v-if="visibale">
+      <slot></slot>
+    </div>
+  </div>
 </template>
 
 <script>
+import Icon from '../icon'
 export default {
-    name:"ikCollapsItem",
-    props:{
-        title:{
-            type: String,
-            required:true
-        }
+  name: "ikCollapsItem",
+  components: {
+    "ik-icon": Icon,
+  },
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    name:{
+      type: String,
+      required: true,
     }
-}
+  },
+  data() {
+    return {
+      visibale: false,
+    };
+  },
+  inject: ["eventBus"],
+  mounted() {
+    this.eventBus && this.eventBus.$on("update:selected",(name)=>{
+      (name !== this.name) && this.onClose()    
+    })
+  },
+  methods:{
+    onClick(){
+      if(this.visibale){
+        this.onClose()
+        
+      }else{
+        this.onOpen()
+        this.eventBus && this.eventBus.$emit('update:selected',this.name)
+      }  
+    },
+    onOpen(){
+      this.visibale = true
+      this.$refs.icon.style.transform='rotate(0)'
+    },
+    onClose(){
+      this.visibale = false
+      this.$refs.icon.style.transform='rotate(90deg)'
+    }
+  }
+};
 </script>
 
 <style lang="scss">
 $border-color: #ddd;
 $border-radius: 4px;
-.ik-collapse-item{
-    > .title{
+.ik-collapse-item {
+  width: 100%;
+  border-bottom: 1px solid $border-color;
+  > .ik-collapse-item__title {
+    display: flex;
+    flex-direction: row;
+    min-height: 32px;
+    align-items: center;
+    >.title-content{
+        flex:1
+    }
+    >.image-wrapper{
         display: flex;
-        min-height: 32px;
-        margin:  -1px;
-        padding:  0 8px;
-        border: 1px solid $border-color;
         align-items: center;
-    }
-    &:first-child{
-        >.title{
-            border-top-left-radius: $border-radius;
-            border-top-right-radius: $border-radius;
+        justify-content: center;
+        >.image-content {
+        transform: scale(.5);   
         }
     }
-    &:last-child{
-        >.title:last-child{
-            border-bottom-left-radius: $border-radius;
-            border-bottom-right-radius: $border-radius;
-        }
-    }
+  }
+  &:last-child {
+    margin-bottom: -1px;
+    // > .ik-collapse-item__title:last-child {
+    //   border-bottom-left-radius: $border-radius;
+    //   border-bottom-right-radius: $border-radius;
+    // }
+  }
+  
 }
 </style>
