c9a1e785116d24bdb6336292ba79838381f9b07f
wynn-w <wwwynn22@gmail.com>
2021-02-01 15:02:10 +0800

    新增：添加 color 部份功能实现（用户可自定义图标颜色）

--- src/components/icon.vue
+++ src/components/icon.vue
@@ -14,13 +14,21 @@ export default {
         return typeof value == "string";
       },
     },
+    color: {
+      type: String,
+      validator(value) {
+        const rule = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/g
+        return typeof value === "string" && rule.test(value)
+      },
+    },
   },
+  mounted(){
+    this.color && (this.$el.style.fill = `${this.color}`)
+  }
 };
 </script>
 
-<style>
-.icon {
-  width: 1em;
-  height: 1em;
-}
+<style lang="scss">
+svg path { fill: inherit !important }
+.icon {  width: 1em; height: 1em; fill: currentColor; }
 </style>
