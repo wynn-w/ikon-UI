623ad372dcdf7d0fde46bfcceeb5430830d74080
wynn-w <wwwynn22@gmail.com>
2021-02-05 00:00:35 +0800

    修改： 改变了配置文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,24 +1,25 @@
 module.exports = {
-    title: 'ikon UI',
-    description: '一个简单的 UI 框架',
-    themeConfig: {
-        nav: [
-            { text: 'Home', link: '/' },
-            { text: 'Guide', link: '/guide/' },
-            { text: 'External', link: 'https://google.com' },
-        ],
-        sidebar: [
+  base: '/ikon',
+  title: 'ikon UI',
+  description: '一个基于 Vue 2.0的 UI 框架,持续学习更新中...',
+  themeConfig: {
+      nav: [
+          { text: '主页', link: '/' },
+          { text: 'Github', link: 'https://github.com/wynn-w/' },
+          { text: '欢迎 star', link: 'https://github.com/wynn-w/ikon-UI' },
+      ],
+      sidebar: [
+          {
+              title: '开发指南',
+              children: ['/install/', '/get-start/',],
+          },
+          {
+              title: '组件',
+              collapsable: false,
+              children: ['/components/icon'],
+              //initialOpenGroupIndex: -1 // 可选的, 默认值是 0
+          }
 
-            {
-                title: '开发指南',
-                children: ['/install/','/get-start/',],
-            },
-            {
-                title: '组件',
-                children: ['/components/icon'],
-                //initialOpenGroupIndex: -1 // 可选的, 默认值是 0
-            }
-
-        ]
-    }
+      ]
+  }
 }
