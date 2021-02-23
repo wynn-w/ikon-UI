87d3502f67cdf5881d76e256a8596c1ac15526ad
wynn-w <wwwynn22@gmail.com>
2021-02-23 18:05:46 +0800

    修改：重写了 input 组件。实现对原生属性（ min、max、step、id、name、placeho lder、maxlength、value、disable、readonly 以及部份type）、change/input/focus/blu r 事件的支持；新增自定义属性 showPassword、cleared、size；修复 focus 状态下边框 不显示问题

--- src/components/form/input.vue
+++ src/components/form/input.vue
@@ -1,27 +1,55 @@
 <template>
-  <div class="wrapper" :class="{ error }">
+  <div class="ik-input" :class="[classSize, classSuffix]">
     <input
-      :value="value"
+      :id="id"
+      :name="name"
+      :value="currentValue"
       :disabled="disabled"
       :readonly="readonly"
-      type="text"
+      :type="currentType"
+      :step="step"
+      :min="min"
+      :max="max"
+      :size="size"
+      :showPassword="showPassword"
+      :clearable="clearable"
+      :maxlength="currentMaxLength"
+      :placeholder="placeholder"
       @change="$emit('change', $event, $event.target.value)"
       @input="$emit('input', $event.target.value)"
       @focus="$emit('focus', $event, $event.target.value)"
-      @blur="$emit('blur', $event,   $event.target.value)"
+      @blur="$emit('blur', $event, $event.target.value)"
     />
-    <template v-if="error">
-      <p class="message">{{ error }}</p>
-    </template>
+    <div class="ik-input__icon-wrapper" ref="ikIputIcon">
+      <div
+        v-if="clearable && currentValue != ''"
+        class="ik-input__icon-wrapper__clear"
+        @click="clearValue"
+      >
+        <ik-icon name="ik-icon-error"></ik-icon>
+      </div>
+      <div
+        v-if="showPassword"
+        @click="changeEye"
+        class="ik-input__icon-wrapper__eye"
+      >
+        <ik-icon :name="icon"></ik-icon>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Icon from "../icon";
 export default {
-  name: "IkInput",
+  name: "ik-input",
+  components: {
+    [Icon.name]: Icon,
+  },
   props: {
     value: {
-      type: String,
+      type: [String, Number],
+      required: true,
     },
     disabled: {
       type: Boolean,
@@ -31,37 +59,213 @@ export default {
       type: Boolean,
       defalut: false,
     },
-    error: {
+    type: {
       type: String,
+      defalut: "text",
+      validator(value) {
+        return ["number", "text", "password"].indexOf(value) > -1;
+      },
+    },
+    size: {
+      type: String,
+      validator(value) {
+        return ["mini", "medium", "small"].indexOf(value) > -1;
+      },
+    },
+    showPassword: {
+      type: Boolean,
+      defalut: false,
+    },
+    clearable: {
+      type: Boolean,
+      defalut: false,
+    },
+    id: String,
+    name: String,
+    placeholder: String,
+    step: {
+      type: [Number, String],
+      validator(value) {
+        if (value - 0 == value && !isNaN(value)) {
+          return value;
+        } else {
+          return console.warn(
+            `值 “${value}” 类型为 ${typeof value}，step 的期待值类型为 Number `
+          );
+        }
+      },
+    },
+    maxlength: {
+      type: [Number, String],
+      validator(value) {
+        if (value - 0 == value && !isNaN(value)) {
+          return value;
+        } else {
+          return console.warn(
+            `值 “${value}” 类型为 ${typeof value}，maxlength 的期待值类型为 Number `
+          );
+        }
+      },
+    },
+    min: {
+      type: [Number, String],
+      validator(value) {
+        if (value - 0 == value && !isNaN(value)) {
+          return value;
+        } else {
+          return console.warn(
+            `值 “${value}” 类型为 ${typeof value}，min 的期待值类型为 Number `
+          );
+        }
+      },
+    },
+    max: {
+      type: [Number, String],
+      validator(value) {
+        if (value - 0 == value && !isNaN(value)) {
+          return value;
+        } else {
+          return console.warn(
+            `值 “${value}” 类型为 ${typeof value}，max 的期待值类型为 Number `
+          );
+        }
+      },
+    },
+  },
+  computed: {
+    getValue() {
+      return this.changeValue();
+    },
+    // getType() {
+    //   return this.changeType();
+    // },
+    classSize() {
+      return this.size ? `ik-input--${this.size}` : "";
+    },
+    classSuffix() {
+      return this.clearable || this.showPassword ? `ik-input--suffix` : "";
+    },
+  },
+  data() {
+    return {
+      icon: `ik-icon-eye-close`,
+      currentType: null,
+      currentValue: null,
+      currentMaxLength: null,
+    };
+  },
+  mounted() {
+    this.hasVModel();
+    this.localtedPorps();
+    this.setIconWrapperProperty();
+  },
+  methods: {
+    hasVModel() {
+      if (this.value === undefined) {
+        this.currentMaxLength = 0;
+        return console.warn(`组件 ${this.$options.name} 未绑定 v-model`);
+      }
+    },
+    setIconWrapperProperty() {
+      this.$refs.ikIputIcon.style.setProperty(
+        "height",
+        `${this.$el.scrollHeight}px`
+      );
+      if (this.readonly || this.disabled) {
+        this.$refs.ikIputIcon.style.setProperty("z-index", -1);
+      }
+    },
+    localtedPorps() {
+      this.value && this.changeValue(this.value);
+      this.type && (this.currentType = this.type);
+      this.showPassword && (this.currentType = "password");
+      this.maxlength && (this.currentMaxLength = this.maxlength);
+    },
+    clearValue() {
+      this.changeValue("");
+      // 通知外部
+      this.$emit("change", this.currentValue);
+      this.$emit("input", this.currentValue);
+      this.$emit("focus", this.currentValue);
+      this.$emit("blur", this.currentValue);
+    },
+    // changeMaxLength(value) {
+    //   this.currentMaxLength = value === 0 ? value : this.maxlength;
+    //   return this.currentMaxLength;
+    // },
+    changeValue(value) {
+      this.currentValue = value === "" ? value : this.value;
+      return this.currentValue;
+    },
+    // changeType(type) {
+    //   this.currentType = type ? type : this.type;
+    //   return this.currentType;
+    // },
+    changeEye() {
+      if (this.icon === "ik-icon-eye-open") {
+        this.icon = "ik-icon-eye-close";
+        this.currentType = "password";
+      } else {
+        this.icon = "ik-icon-eye-open";
+        this.size
+          ? (this.currentType = this.size)
+          : (this.currentType = "text");
+      }
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-$height: 2.6667em;
-$border-color: #999;
-$border-color-hover: #409eff;
-$border-radius: 0.3333em;
-$font-size: 1em;
+$height: 36px;
+$height-medium: 34px;
+$height-small: 32px;
+$height-mini: 28px;
+$border-color: #dcdfe6;
+$border-radius: 4px;
+$font-size: 14px;
 $box-shadow-color: #9edbff;
 $red: #f1453e;
-.wrapper {
+.ik-input {
   font-size: $font-size;
-  display: inline-block;
+  display: inline-flex;
+  flex-direction: row;
+  height: $height;
+  outline: none;
+  font-size: $font-size;
+  transition: all 0.1s;
+  position: relative;
+  cursor: pointer;
+
+  &.ik-input--medium {
+    height: $height-medium;
+  }
+  &.ik-input--small {
+    height: $height-small;
+  }
+  &.ik-input--mini {
+    height: $height-mini;
+  }
+  &.ik-input--suffix {
+    > input {
+      padding-right: 44px;
+    }
+  }
   > input {
+    padding: 0 12px;
+    border: 1px solid $border-color;
+    border-radius: 4.6662px;
+    color: #606266;
+    background: #ffffff;
     outline: none;
-    height: $height;
-    border: 0.0833em solid $border-color;
-    border-radius: 0.3333em;
-    padding: 0 0.6667em;
-    font-size: inherit;
+    flex: 1;
     &:hover {
-      border-color: $border-color-hover;
+      border-color: #c0c4cc;
     }
     &:focus {
-      border-color: $border-color-hover;
-      box-shadow: 0 0 0.25em $box-shadow-color;
+      border-color: #409eff;
+      outline: none;
+      box-shadow: 0 0 3.5px $box-shadow-color;
     }
     &[disabled],
     &[readonly] {
@@ -70,17 +274,30 @@ $red: #f1453e;
       cursor: not-allowed;
     }
   }
-  &.error {
-    > input {
-      border-color: $red;
-      &:focus {
-        box-shadow: 0 0 0.25em $red;
-      }
+}
+.ik-input__icon-wrapper {
+  position: absolute;
+  height: 100%;
+  right: 5px;
+  top: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  > .ik-input__icon-wrapper__clear,
+  .ik-input__icon-wrapper__eye {
+    margin: 0 4px;
+    transition: all 0.1s;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #c0c4cc;
+    &:hover,
+    &:focus {
+      color: #909399;
     }
-  }
-  .message {
-    font-size: 0.8333rem;
-    color: $red;
   }
 }
 </style>
