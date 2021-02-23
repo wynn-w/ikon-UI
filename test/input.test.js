e9a78e9e932f10fcc4245a8a188d5603b9a8ff26
wynn-w <wwwynn22@gmail.com>
2021-02-23 18:06:16 +0800

    修改：更新了测试用例

--- test/input.test.js
+++ test/input.test.js
@@ -21,13 +21,13 @@ describe('Input', () => {
           value: 'value'
         }
       }).$mount()
-      const useElement = vm.$el.querySelector('input')
-      expect(useElement.value).to.equal('value')
+      expect(vm.value).to.equal('value')
     })
 
     it('可以设置disabled', () => {
       vm = new Constructor({
         propsData: {
+          value: 'value',
           disabled: true
         }
       }).$mount()
@@ -38,6 +38,7 @@ describe('Input', () => {
     it('可以设置readonly', () => {
       vm = new Constructor({
         propsData: {
+          value: 'value',
           readonly: true
         }
       }).$mount()
@@ -45,17 +46,131 @@ describe('Input', () => {
       expect(useElement.readOnly).to.equal(true)
     })
 
-    it('可以设置 error 样式', () => {
+    it('可以设置 type.', () => {
       vm = new Constructor({
         propsData: {
-          error: "错误"
+          value: 'value',
+          type: 'password'
+        }
+      }).$mount()
+      // 好像不和情理了
+      expect(vm.currentType).to.equal('password')
+    })
+
+    it('可以设置 size.', () => {
+      vm = new Constructor({
+        propsData: {
+          value: 'value',
+          size: 'medium',
+        }
+      }).$mount()
+      const useElement = vm.$el
+      expect(useElement.classList.contains("ik-input--medium")).is.true
+    })
+    it('可以设置 showPassword，且图标初始状态为 ik-icon-eye-close.', (done) => {
+      vm = new Constructor({
+        propsData: {
+          value: 'value',
+          showPassword: true,
+        }
+      }).$mount()
+      const iconWrapper = vm.$el.querySelector('.ik-input__icon-wrapper__eye')
+      const input = vm.$el.getElementsByTagName('input')[0]
+      const icon = vm.$el.querySelector('use')
+      expect(icon.getAttribute('xlink:href')).to.equal('#ik-icon-eye-close')
+      expect(vm.currentType).to.equal('password')
+      iconWrapper.click()
+      setTimeout(() => {
+        expect(icon.getAttribute('xlink:href')).to.equal('#ik-icon-eye-open')
+        expect(input.type).to.equal('text')
+        done()
+      })
+    })
+    it('可以设置 clearable，且点击后 value 会被清空', (done) => {
+      vm = new Constructor({
+        propsData: {
+          value: 'value',
+          clearable: true,
+        }
+      }).$mount()
+      const iconWrapper = vm.$el.querySelector('.ik-input__icon-wrapper__clear')
+      const icon = vm.$el.querySelector('use')
+      expect(icon.getAttribute('xlink:href')).to.equal('#ik-icon-error')
+      expect(vm.value).to.equal('value')
+      iconWrapper.click()
+      setTimeout(() => {
+        expect(vm.currentValue).to.equal('')
+        done()
+      }, 100)
+    })
+    it('可以设置 size.', () => {
+      vm = new Constructor({
+        propsData: {
+          value: 'value',
+          id: 'id1',
         }
       }).$mount()
       const useElement = vm.$el.querySelector('input')
-      const useElementP = vm.$el.querySelector('p')
-      const useElementParent = useElement.parentNode
-      expect(useElementParent.classList.contains('error')).to.equal(true)
-      expect(useElementP.textContent).to.equal('错误')
+      expect(useElement.id).to.equal("id1")
+    })
+    it('可以设置 name.', () => {
+      vm = new Constructor({
+        propsData: {
+          value: 'value',
+          name: 'name',
+        }
+      }).$mount()
+      const useElement = vm.$el.querySelector('input')
+      expect(useElement.name).to.equal("name")
+    })
+    it('可以设置 placeholder.', () => {
+      vm = new Constructor({
+        propsData: {
+          value: 'value',
+          placeholder: 'placeholder',
+        }
+      }).$mount()
+      const useElement = vm.$el.querySelector('input')
+      expect(useElement.placeholder).to.equal("placeholder")
+    })
+    it('可以设置 step.', () => {
+      vm = new Constructor({
+        propsData: {
+          value: 'value',
+          step: '2',
+        }
+      }).$mount()
+      const useElement = vm.$el.querySelector('input')
+      expect(useElement.step).to.equal("2")
+    })
+    it('可以设置 maxlength.', () => {
+      vm = new Constructor({
+        propsData: {
+          value: 'value',
+          maxlength: '9',
+        }
+      }).$mount()
+      expect(vm.currentMaxLength).to.equal("9")
+    })
+    it('可以设置 step.', () => {
+      vm = new Constructor({
+        propsData: {
+          value: 'value',
+          min: '2',
+        }
+      }).$mount()
+      const useElement = vm.$el.querySelector('input')
+      expect(useElement.min).to.equal("2")
+    })
+    it('可以设置 max.', () => {
+      vm = new Constructor({
+        propsData: {
+          value: 'value',
+          max: '2',
+        }
+      }).$mount()
+      const useElement = vm.$el.querySelector('input')
+      expect(useElement.max).to.equal("2")
     })
   })
   describe('event', () => {
