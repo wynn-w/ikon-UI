4df65583a433cb4742bbad845f5488db1fed445c
wynn-w <wwwynn22@gmail.com>
2021-03-01 22:03:47 +0800

    修改：更新了vuepress 的配置文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,8 +2,22 @@ module.exports = {
     base: '/ikon-UI/',
     title: 'ikon UI',
     description: '一个基于 Vue 2.0的 UI 框架,持续学习更新中...',
-    plugins: ['demo-container'],
+    plugins: ['demo-container',
+
+    ],
     themeConfig: {
+        search: true,
+        locales: [
+            {
+                "lang": "zh-CN",
+                "demo-block": {
+                    "hide-text": "隐藏代码",
+                    "show-text": "显示代码",
+                    "copy-text": "复制代码",
+                    "copy-success": "复制成功"
+                }
+            }
+        ],
         nav: [
             { text: '主页', link: '/' },
             { text: 'Github', link: 'https://github.com/wynn-w/' },
@@ -11,21 +25,29 @@ module.exports = {
         ],
         sidebar: [
             {
+                title: '介绍',
+                collapsable: false,
+                children: [
+                    '/introduce/',
+                ],
+            },
+            {
                 title: '开发指南',
-                children: ['/install/', '/get-start/',],
+                collapsable: false,
+                children: ['/install/',],
             },
             {
                 title: '组件',
                 collapsable: false,
                 children: [
-                    '/components/icon', 
-                    '/components/button', 
-                    '/components/input', 
-                    '/components/layout', 
-                    '/components/tabs', 
-                    '/components/collapse', 
-                    '/components/grid', 
-                    '/components/toast', 
+                    '/components/icon',
+                    '/components/button',
+                    '/components/input',
+                    '/components/layout',
+                    '/components/tabs',
+                    '/components/collapse',
+                    '/components/grid',
+                    '/components/toast',
                     '/components/popover'],
                 //initialOpenGroupIndex: -1 // 可选的, 默认值是 0
             }
