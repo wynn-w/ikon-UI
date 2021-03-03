f80dd81a22996047802a6c33f17ae1a22e4e302c
wynn-w <wwwynn22@gmail.com>
2021-03-03 18:09:21 +0800

    新增：添加了部署 gitee 的文件

---
+++ deploy.gitee.sh
@@ -0,0 +1,26 @@
+@echo off
+        
+#REM 构建生成静态文件
+echo building...
+call build.bat
+echo building-complete
+        
+#REM 进入生成的文件夹
+cd docs/.vuepress/dist
+        
+#REM 如果是发布到自定义域名
+#REM echo 'www.example.com' > CNAME
+        
+git init
+git add -A
+git commit -m 'deploy'
+        
+#REM 如果发布到 https://gitee.com/<USERNAME>
+#REM git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+        
+#REM 如果发布到 https://gitee.com/<USERNAME>/<REPO>
+git push -f https://gitee.com/wynn-w/ikon-UI.git master:gh-pages       
+cd ..
+cd ..
+cd ..
+echo Auto-Deploy-Complete
